ea3ad505782a4e018c98ea0910767728b1cb56f1
Thiago <thlindustries23@gmail.com>
2019-10-14 13:38:55 -0300

    Post feito

--- frontend-obelisco/src/app/components/cadastro/cadastro.component.ts
+++ frontend-obelisco/src/app/components/cadastro/cadastro.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { EnderecoModel } from 'src/app/shared/models/Endereco.model';
 import { RefsService } from 'src/app/shared/helpers/refs.service';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-cadastro',
@@ -46,7 +47,23 @@ export class CadastroComponent implements OnInit {
     if (this.form.valid) {
       const model = this.form.getRawValue() as EnderecoModel;
 
-      console.log(model);
+      var dados= {model:model};//{no:nome,em:email,id:idade,cu:curso,tel:telefone,ce:cep,lo:logradouro,ba:bairro,loc:localidade,nu:numero};
+
+      try {
+          $.post('http://192.168.240.203:8000/model.php',dados,function(data)
+          {
+              var obj = jQuery.parseJSON(data);
+              //var obj = data;
+              //$("#teste").text(""+obj);
+              console.log(obj);
+             
+          });
+        }
+        catch(err) {
+          //$("#teste").text(err.message);
+        }
+
+      
     }
   }
 
