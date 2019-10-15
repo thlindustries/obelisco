6ce18566afa9abf6e675d96a364352dc1c0f0960
Thiago <thlindustries23@gmail.com>
2019-10-15 11:51:48 -0300

    Type script FDP

---
+++ frontend-obelisco/proxy.config.js
@@ -0,0 +1,8 @@
+const proxy = [
+    {
+      context: '/api',
+      target: 'http://192.168.240.203:8000',
+      pathRewrite: {'^/api' : ''}
+    }
+  ];
+  module.exports = proxy;
--- frontend-obelisco/src/app/components/cadastro/cadastro.component.ts
+++ frontend-obelisco/src/app/components/cadastro/cadastro.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { EnderecoModel } from 'src/app/shared/models/Endereco.model';
 import { RefsService } from 'src/app/shared/helpers/refs.service';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl,FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as $ from 'jquery';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-cadastro',
@@ -10,6 +13,11 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class CadastroComponent implements OnInit {
 
+  SERVER_URL = "http://192.168.240.203:8000/model.php";
+  //uploadForm: FormGroup; 
+
+  private formBuilder: FormBuilder; 
+  private httpClient: HttpClient;
   public initialModel: EnderecoModel;
   public form: FormGroup;
   public formControls: {
@@ -25,7 +33,20 @@ export class CadastroComponent implements OnInit {
 
   ngOnInit() {
     this.buildForm(new EnderecoModel());
+    
+    // this.form = this.formBuilder.group({
+    //   profile: ['']
+    // });
+    // this.uploadForm = this.formBuilder.group({
+    //   profile: ['']
+    // });
   }
+  onFileSelect(event) {
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      this.form.get('profile').setValue(file);
+    }
+  }   
 
   public buildForm(model: EnderecoModel): void {
     this.initialModel = model;
@@ -40,19 +61,50 @@ export class CadastroComponent implements OnInit {
     };
 
     this.form = this.refs.formBuilder.group(this.formControls);
+  
   }
+  
 
-  public onSubmit(): void {
+  public onSubmit() : void {
     if (this.form.valid) {
       const model = this.form.getRawValue() as EnderecoModel;
 
-      const dados = { model };
+      //const dados = { model };
       // {no:nome,em:email,id:idade,cu:curso,tel:telefone,ce:cep,lo:logradouro,ba:bairro,loc:localidade,nu:numero};
 
-      this.refs.cadastroService.post(dados).subscribe(
+      // const formData = new FormData();
+      // formData.append('file', this.form.get('profile').value);
+
+      // this.httpClient.post<any>(this.SERVER_URL, model).subscribe(
+      //   (res) => console.log(res),
+      //   (err) => console.log(err)
+      // );
+      //var dados = {model:model};
+      //console.log(dados);
+      // {no:nome,em:email,id:idade,cu:curso,tel:telefone,ce:cep,lo:logradouro,ba:bairro,loc:localidade,nu:numero};
+
+      this.refs.cadastroService.post(model).subscribe(
         res => console.log(res),
         err => console.log(err)
       );
+      // this.httpClient.post(this.SERVER_URL,model).subscribe(
+      //   res => console.log(res),
+      //   err => console.log(err)
+      // );
+
+      // try {
+      //   $.post('http://192.168.240.203:8000/model.php',dados,function(data)
+      //   {
+      //       var obj = jQuery.parseJSON(data);
+      //       //var obj = data;
+      //       //$("#teste").text(""+obj);
+      //       console.log(obj);
+
+      //   });
+      // }
+      // catch(err) {
+      //   //$("#teste").text(err.message);
+      // }
     }
   }
 
@@ -75,3 +127,4 @@ export class CadastroComponent implements OnInit {
     }
   }
 }
+
