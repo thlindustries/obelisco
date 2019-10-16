2140214df99f70e247341282730f39b1b6b01902
Wintertaid <32654571+apedroh@users.noreply.github.com>
2019-10-15 23:28:26 -0300

    Form Completo

--- frontend-obelisco/src/app/components/cadastro/cadastro.component.ts
+++ frontend-obelisco/src/app/components/cadastro/cadastro.component.ts
@@ -1,10 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EnderecoModel } from 'src/app/shared/models/Endereco.model';
 import { RefsService } from 'src/app/shared/helpers/refs.service';
-import { FormControl,FormBuilder, FormGroup, Validators } from '@angular/forms';
-import * as $ from 'jquery';
-import { HttpClient } from '@angular/common/http';
-
+import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
@@ -13,14 +10,17 @@ import { HttpClient } from '@angular/common/http';
 })
 export class CadastroComponent implements OnInit {
 
-  SERVER_URL = "http://192.168.240.203:8000/model.php";
-  //uploadForm: FormGroup; 
-
-  private formBuilder: FormBuilder; 
-  private httpClient: HttpClient;
   public initialModel: EnderecoModel;
   public form: FormGroup;
+
   public formControls: {
+    nome: FormControl,
+    email: FormControl,
+    idade: FormControl,
+    curso: FormControl,
+    telefone: FormControl,
+
+    numero: FormControl,
     cep: FormControl,
     logradouro: FormControl,
     complemento: FormControl,
@@ -33,25 +33,19 @@ export class CadastroComponent implements OnInit {
 
   ngOnInit() {
     this.buildForm(new EnderecoModel());
-    
-    // this.form = this.formBuilder.group({
-    //   profile: ['']
-    // });
-    // this.uploadForm = this.formBuilder.group({
-    //   profile: ['']
-    // });
   }
-  onFileSelect(event) {
-    if (event.target.files.length > 0) {
-      const file = event.target.files[0];
-      this.form.get('profile').setValue(file);
-    }
-  }   
 
   public buildForm(model: EnderecoModel): void {
     this.initialModel = model;
 
     this.formControls = {
+      nome: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      idade: new FormControl('', Validators.required),
+      curso: new FormControl('', Validators.required),
+      telefone: new FormControl('', Validators.required),
+
+      numero: new FormControl('', Validators.required),
       cep: new FormControl('', Validators.required),
       logradouro: new FormControl({ value: undefined, disabled: true }),
       complemento: new FormControl({ value: undefined, disabled: true }),
@@ -61,50 +55,15 @@ export class CadastroComponent implements OnInit {
     };
 
     this.form = this.refs.formBuilder.group(this.formControls);
-  
   }
-  
 
-  public onSubmit() : void {
+
+  public onSubmit(): void {
     if (this.form.valid) {
-      const model = this.form.getRawValue() as EnderecoModel;
-
-      //const dados = { model };
-      // {no:nome,em:email,id:idade,cu:curso,tel:telefone,ce:cep,lo:logradouro,ba:bairro,loc:localidade,nu:numero};
-
-      // const formData = new FormData();
-      // formData.append('file', this.form.get('profile').value);
-
-      // this.httpClient.post<any>(this.SERVER_URL, model).subscribe(
-      //   (res) => console.log(res),
-      //   (err) => console.log(err)
-      // );
-      //var dados = {model:model};
-      //console.log(dados);
-      // {no:nome,em:email,id:idade,cu:curso,tel:telefone,ce:cep,lo:logradouro,ba:bairro,loc:localidade,nu:numero};
-
-      this.refs.cadastroService.post(model).subscribe(
+      this.refs.cadastroService.post(this.form.getRawValue()).subscribe(
         res => console.log(res),
         err => console.log(err)
       );
-      // this.httpClient.post(this.SERVER_URL,model).subscribe(
-      //   res => console.log(res),
-      //   err => console.log(err)
-      // );
-
-      // try {
-      //   $.post('http://192.168.240.203:8000/model.php',dados,function(data)
-      //   {
-      //       var obj = jQuery.parseJSON(data);
-      //       //var obj = data;
-      //       //$("#teste").text(""+obj);
-      //       console.log(obj);
-
-      //   });
-      // }
-      // catch(err) {
-      //   //$("#teste").text(err.message);
-      // }
     }
   }
 
--- frontend-obelisco/src/app/shared/services/http-client.service.ts
+++ frontend-obelisco/src/app/shared/services/http-client.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Model } from '../models/Model.model';
+import { map, catchError, retry } from 'rxjs/operators';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -16,23 +17,39 @@ export class HttpClientService<T extends Model> {
 
   constructor(private http: HttpClient) { }
 
-  getAll(): Observable<T[]> {
-    return this.http.get<T[]>(`${this.serviceUrl}`);
+  public getAll(): Observable<T[]> {
+    return this.http.get<T[]>(`${this.serviceUrl}`)
+      .pipe(map((res: any) => res.json()), catchError(this.errorHandle));
   }
 
-  get(id: string): Observable<T> {
-    return this.http.get<T>(`${this.serviceUrl}/${id}`);
+  public get(id: string): Observable<T> {
+    return this.http.get<T>(`${this.serviceUrl}/${id}`)
+      .pipe(map((res: any) => res.json()), catchError(this.errorHandle));
   }
 
-  post(data: T): Observable<T> {
-    return this.http.post<T>(`${this.serviceUrl}`, data, httpOptions);
+  public post(data: T): Observable<T> {
+    return this.http.post<T>(`${this.serviceUrl}`, JSON.stringify(data), httpOptions)
+      .pipe(map((res: any) => res.json()), catchError(this.errorHandle));
   }
 
-  put(data: T): Observable<T> {
-    return this.http.put<T>(`${this.serviceUrl}/${data.id}`, data, httpOptions);
+  public put(data: T): Observable<T> {
+    return this.http.put<T>(`${this.serviceUrl}/${data.id}`, JSON.stringify(data), httpOptions)
+      .pipe(map((res: any) => res.json()), catchError(this.errorHandle));
   }
 
-  delete(id: string): Observable<T> {
-    return this.http.delete<T>(`${this.serviceUrl}/${id}`);
+  public delete(id: string): Observable<T> {
+    return this.http.delete<T>(`${this.serviceUrl}/${id}`)
+      .pipe(map((res: any) => res.json()), catchError(this.errorHandle));
+  }
+
+  public errorHandle(error): Observable<never> {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = error.error.message;
+    } else {
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
   }
 }
